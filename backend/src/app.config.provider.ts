08fa6986849b8209f867841962ab36ce37391e60
z-u-s-h-o <zigy0ygiz@yandex.ru>
2026-06-19 22:39:01 +0500

    fix: apply review comments

--- backend/src/app.config.provider.ts
+++ backend/src/app.config.provider.ts
@@ -14,12 +14,21 @@ export interface AppConfigDatabase {
 export const configProvider = {
   provide: 'CONFIG',
   useFactory: (configService: ConfigService) => {
+    const databaseDriver = configService.get<string>('DATABASE_DRIVER');
+    const databaseHost = configService.get<string>('DATABASE_HOST');
+    const databasePort = configService.get<string>('DATABASE_PORT');
+    const databaseName = configService.get<string>('DATABASE_NAME');
+    const databaseUsername = configService.get<string>('DATABASE_USERNAME');
+    const databasePassword = configService.get<string>('DATABASE_PASSWORD');
+
+    const databaseUrl = `postgres://${databaseUsername}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}`;
+
     return {
       database: {
-        type: configService.get<string>('DATABASE_DRIVER'),
-        url: configService.get<string>('DATABASE_URL'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
+        type: databaseDriver,
+        url: databaseUrl,
+        username: databaseUsername,
+        password: databasePassword,
       },
     } as AppConfig;
   },
